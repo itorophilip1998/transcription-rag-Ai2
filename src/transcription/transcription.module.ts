15c16a55ad98d065b562681b2ed340b052b4632f
Itorophilip1998 <itorophilip1998@gmail.com>
2025-01-11 11:13:42 +0100

    Update script

--- src/transcription/transcription.module.ts
+++ src/transcription/transcription.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TranscriptionController } from './transcription.controller';
+import { TranscriptionService } from './transcription.service';
 
 @Module({})
-export class TranscriptionModule {}
+export class TranscriptionModule {
+  imports: [];
+  controllers: [TranscriptionController];
+  providers: [TranscriptionService];
+}
