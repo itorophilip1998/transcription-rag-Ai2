b787a4ee9b059a43398b15c168b036acb778ce7d
Itorophilip1998 <itorophilip1998@gmail.com>
2025-01-11 11:09:36 +0100

    save

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  // Enable static file serving for uploaded files (optional)
-  app.useStaticAssets('uploads', { prefix: '/uploads' });
-
   await app.listen(3000);
 }
 bootstrap();
